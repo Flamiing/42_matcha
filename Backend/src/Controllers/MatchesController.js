09e686e8d54862b32d2d3047a5cc6ba131af8065
Flamiing <amirlaaouam@gmail.com>
2025-01-27 00:28:31 +0100

    get all matches endpoint now returns the matches with the needed information

--- Backend/src/Controllers/MatchesController.js
+++ Backend/src/Controllers/MatchesController.js
@@ -1,5 +1,7 @@
 // Local Imports:
 import matchesModel from '../Models/MatchesModel.js';
+import userModel from '../Models/UserModel.js';
+import { returnErrorStatus } from '../Utils/errorUtils.js';
 import StatusMessage from '../Utils/StatusMessage.js';
 
 export default class MatchesController {
@@ -24,10 +26,38 @@ export default class MatchesController {
                 .status(500)
                 .json({ msg: StatusMessage.INTERNAL_SERVER_ERROR });
 
-        const matches = [...matchesOne, ...matchesTwo];
+        const rawMatches = [...matchesOne, ...matchesTwo];
 
-        //const matchesWithInfo = await MatchesController.getMatchesInfo()
+        const matches = await MatchesController.getMatchesInfo(req, res, rawMatches);
+        if (!matches) return res;
 
         return res.json({ msg: matches });
+    }
+
+    static async getMatchesInfo(req, res, rawMatches) {
+        const { API_HOST, API_PORT, API_VERSION } = process.env;
+
+        let matches = [];
+
+        for (const match of rawMatches) {
+            let id = null;
+            let user = null;
+            if (match.user_id_1 !== req.session.user.id) id = match.user_id_1;
+            else id = match.user_id_2;
+            user = await userModel.getById({ id });
+            if (!user || user.length === 0) return returnErrorStatus(res, 500, StatusMessage.INTERNAL_SERVER_ERROR);
+
+            const profilePictureURL = `http://${API_HOST}:${API_PORT}/api/v${API_VERSION}/users/${id}/profile-picture`
+
+            const newMatch = {
+                userId: id,
+                username: user.username,
+                profilePicture: profilePictureURL,
+                matchId: match.id
+            }
+            matches.push(newMatch);
+        }
+
+        return matches;
     }
 }
