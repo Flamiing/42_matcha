d57947984805c50c40a16c2b887a53925a017f13
Flamiing <amirlaaouam@gmail.com>
2025-01-26 20:01:16 +0100

    some additions for the events implementation

--- Backend/src/Schemas/eventSchema.js
+++ Backend/src/Schemas/eventSchema.js
@@ -7,9 +7,9 @@ import StatusMessage from '../Utils/StatusMessage.js';
 const eventSchema = z.object({
     description: z
             .string({
-                invalid_type_error: 'Invalid biography.',
+                invalid_type_error: 'Invalid description.',
             })
-            .max(500, 'Biography must be 500 characters or fewer.')
+            .max(500, 'Description must be 500 characters or fewer.')
             .optional(),
     new_password: z
         .string({
