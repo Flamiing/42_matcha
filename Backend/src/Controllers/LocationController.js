f86e98ca0d3e0ac79d28b9efe18359d64c572f40
Flamiing <amirlaaouam@gmail.com>
2025-02-15 04:32:45 +0100

    edit user location endpoint implementation

---
+++ Backend/src/Controllers/LocationController.js
@@ -0,0 +1,23 @@
+// Local Imports:
+import userLocationModel from '../Models/UserLocationModel.js';
+import StatusMessage from '../Utils/StatusMessage.js';
+
+export default class LocationController {
+    static async updateUserLocation(req, res) {
+        const { id } = req.session.user;
+        const { location } = req.body;
+
+        const userLocationUpdate = await userLocationModel.update(
+            location,
+            id
+        );
+        if (!userLocationUpdate)
+            return res.status(500).json({ msg: StatusMessage.QUERY_ERROR });
+        if (userLocationUpdate.length === 0)
+            return res
+                .status(404)
+                .json({ msg: StatusMessage.USER_NOT_FOUND });
+        
+        return res.json({ msg: location })
+    }
+}
