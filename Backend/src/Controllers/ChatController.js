62af94c966f6ffa608c706343f9767306c0789b9
Flamiing <amirlaaouam@gmail.com>
2025-02-20 13:35:38 +0100

    feat: backend chat implementation

---
+++ Backend/src/Controllers/ChatController.js
@@ -0,0 +1,7 @@
+// Local Imports:
+import StatusMessage from '../Utils/StatusMessage.js';
+
+export default class ChatController {
+    static async handleChat(req, res) {
+    }
+}
---
+++ Backend/src/Sockets/socketHandler.js
@@ -0,0 +1,9 @@
+export default function socketHandler(io) {
+    io.on('connection', (socket) => {
+        console.log(`New socket connected: ${socket.id}`);
+
+        socket.on('disconnect', () => {
+            console.log(`Socket disconnected: ${socket.id}`)
+        })
+    })
+}
