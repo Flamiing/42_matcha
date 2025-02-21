6efadf3350391b5307744ab9ed777a7870a4b548
Flamiing <amirlaaouam@gmail.com>
2025-02-21 16:13:18 +0100

    changes

--- Backend/src/Sockets/SocketHandler.js
+++ Backend/src/Sockets/SocketHandler.js
@@ -1,11 +1,13 @@
 // Third-Party Imports:
 import { Server } from 'socket.io';
+import cookieParser from 'cookie-parser';
 
 export default class SocketHandler {
     constructor(server) {
         this.io = new Server(server, {
             cors: {
                 origin: '*',
+                credentials: true
             },
         });
 
@@ -15,6 +17,12 @@ export default class SocketHandler {
     #handleSocket() {
         this.io.on('connection', (socket) => {
             console.log(`New socket connected: ${socket.id}`);
+
+            const cookies = socket.request.headers.cookie;
+
+            //const parsedCookies = cookieParser.JSONCookies(cookieParser.signedCookies(socket.request.cookies, 'tu_secreto'));
+
+            console.log('Cookies recibidas:', cookies);
 
             socket.on('disconnect', () => {
                 console.log(`Socket disconnected: ${socket.id}`);
