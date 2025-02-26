5989ffe1531dc54b40e0f2ac71bdf90e4429ce1e
magnitopic <magnitopic@gmail.com>
2025-02-26 23:42:52 +0100

    socket context

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { AuthProvider } from "./context/AuthContext";
+import { SocketProvider } from "./context/SocketContext";
 
 function App() {
 	return (
 		<AuthProvider>
-			<RouterProvider
-				future={{ v7_startTransition: true }}
-				router={router}
-			/>
+			<SocketProvider>
+				<RouterProvider
+					future={{ v7_startTransition: true }}
+					router={router}
+				/>
+			</SocketProvider>
 		</AuthProvider>
 	);
 }
