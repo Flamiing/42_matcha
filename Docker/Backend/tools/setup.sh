f58bf87a08cd351b253338b1b62293710d7d050d
Flamiing <amirlaaouam@gmail.com>
2025-07-25 16:42:29 +0200

    create users folder within the uploads folder

--- Docker/Backend/tools/setup.sh
+++ Docker/Backend/tools/setup.sh
@@ -2,10 +2,14 @@
 
 npm install
 
+# Loads fixture if in development mode
 if [ "$BACKEND_NODE_ENV" = "development" ]; then
   echo "Loading fixtures..."
   npm run fixtures
   echo "Fixtures loaded successfully!"
 fi
+
+# Creates folder for user uploads if it does not exist
+mkdir -p /uploads/users
 
 npm run dev
